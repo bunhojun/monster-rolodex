c20ca65f8efecb4424c14f76af4da8bf9b332b00
Hojun Bun <ponnponn.p@gmail.com>
2020-03-17 21:08:37 +0900

    Updates

---
+++ precache-manifest.04062f4a655ae6efbc3ca77dddb631af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42ded00d3190ecd875679ea5b14c873",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6531784ca12eaf0ce5aa",
+    "url": "/monsters-rolodex/static/css/main.9ed14e52.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6531784ca12eaf0ce5aa",
+    "url": "/monsters-rolodex/static/js/main.234c8c90.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
